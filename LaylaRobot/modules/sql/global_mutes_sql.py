d890834af33721021ea3a5d799d346ef4a287ad0
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-07 07:19:36 +0500

    Update global_mutes_sql.py

--- LaylaRobot/modules/sql/global_mutes_sql.py
+++ LaylaRobot/modules/sql/global_mutes_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from cinderella.modules.sql import BASE, SESSION
+from LaylaRobot.modules.sql import BASE, SESSION
 
 
 class GloballyMutedUsers(BASE):
